b843b9f2a26403fc2ca35116c7dc488e116863a1
HoangHung <hoanghunght2304@gmail.com>
2019-09-20 22:30:13 +0700

    Template engines

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Wolrd!'));
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.get('/users', (req, res) => res.send('User list'));
+app.get('/', (req, res) => res.render('index', {name: 'AAA'}));
+
+app.get('/users', (req, res) => res.render('users/index', {
+    users: [
+        {id: 1, name: 'Hung'},
+        {id: 2, name: 'Huong'}     
+    ]
+}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
