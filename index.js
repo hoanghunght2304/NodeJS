592790ccf00c3145fb3d212f92f2eb193c86ef55
HoangHung <hoanghunght2304@gmail.com>
2019-09-22 15:18:42 +0700

    Query parameters

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+const users = [
+        {id: 1, name: 'Hung'},
+        {id: 2, name: 'Huong'}       
+];
+
 app.get('/', (req, res) => res.render('index', {name: 'AAA'}));
 
 app.get('/users', (req, res) => res.render('users/index', {
-    users: [
-        {id: 1, name: 'Hung'},
-        {id: 2, name: 'Huong'}     
-    ]
+    users: users
 }));
+
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let matchedUsers = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1 );
+    res.render('users/index', {
+        users: matchedUsers
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
