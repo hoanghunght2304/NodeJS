e5657eb8184e38df892818014097957ad1239da1
HoangHung <hoanghunght2304@gmail.com>
2019-09-23 12:58:59 +0700

    POST method

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const users = [
         {id: 1, name: 'Hung'},
@@ -23,6 +26,13 @@ app.get('/users/search', (req, res) => {
     res.render('users/index', {
         users: matchedUsers
     });
+});
+
+app.get('/users/create', (req, res) => res.render('users/create'));
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    res.redirect('/users');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
